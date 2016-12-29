0a4bea2a8234232dfc93196cf2c29b6cd9cfa1d7
刘慧珍 <huizhen.liu@hand-china.com>
2016-12-29 21:59:33 +0800

    readme

--- Exam-12698-20161229-1/src/main/java/com/hand/event/MyListener.java
+++ Exam-12698-20161229-1/src/main/java/com/hand/event/MyListener.java
@@ -1,19 +1,30 @@
-package com.hand.event;
+﻿package com.hand.event;
 
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyListener implements ApplicationListener<ApplicationEvent>{
 
 	public void onApplicationEvent(ApplicationEvent event) {
+		//"Before Insert Film Data" "After Insert Film Data"
 		//判断事件为MyEvent时候执行
         if(event instanceof BeforeEvent){
-            System.out.println("Before Insert Film Data");
-        }else if(event instanceof AfterEvent){
-        	System.out.println("After Insert Film Data");
+            System.out.println(event.getSource());
+        }
+        if(event instanceof AfterEvent){
+        	System.out.println(event.getSource());
+        } 
+        if(event instanceof ContextRefreshedEvent){
+            System.out.println("Context Start");
+        }
+        if(event instanceof ContextClosedEvent){
+            System.out.println("Context Stop");
         }
 	
 	}
 }
+
